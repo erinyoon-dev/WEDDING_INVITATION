a9cda7abcac1bf85f0e2ea98843c46815e2b5d92
erinyoon-dev <53050919+erinyoon-dev@users.noreply.github.com>
2024-08-21 00:54:12 +0900

    Update gallery.jsx

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -20,7 +20,7 @@ import GalleryPhoto12 from "../assets/images_12.jpeg";
 
 const Wrapper = styled.div`
   padding-top: 42px;
-  width: 60%;
+  height: 60%;
   object-fit: contain;
   margin: 0 auto;
 `;
