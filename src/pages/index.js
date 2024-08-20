93a2c1e97e52f0f17ae5164b13269fbb767f421d
erinyoon-dev <53050919+erinyoon-dev@users.noreply.github.com>
2024-08-21 00:04:36 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import "antd/dist/antd.css";
 import Gallery from "../components/gallery";
 import Share from "../components/share";
 import Quote from "../components/quote";
-import Event from "../components/event";
 import "../styles/index.css";
 
 import GroovePaper from "../assets/GroovePaper.png";
@@ -17,6 +16,7 @@ import Iglesia from "../assets/iglesia.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "../components/hero.css";
+import "../components/event.css";
 
 // markup
 const { Footer } = Layout;
@@ -63,19 +63,34 @@ const IndexPage = () => {
         </div>
       </div>
     /* Event */
-      <Event
-        id="evento"
-        title="LA BODA"
-        desc="¡El gran día!"
-        img={<img className="event-image" src={Iglesia}></img>}
-        infoTitle="La Ceremonia"
-        schedule="12:30h, Sábado, 17 de Octubre de 2020"
-        location="Basílica de los Santos Mártires Justo y Pastor"
-        address="Plaça de Sant Just, s/n, 08002 Barcelona"
-        mobility="Se facilitará servicio de autocar desde el centro de Barcelona a la Iglesia y, después de la ceremonia, desde la Iglesia a la Masía Ribas."
-        map="Mapa e indicaciones"
-        mapLink="https://www.google.com/maps/place/Bas%C3%ADlica+dels+Sants+M%C3%A0rtirs+Just+i+Pastor/@41.3828179,2.1760105,17z/data=!3m1!4b1!4m5!3m4!1s0x12a4a2f8dc412f27:0x10cb34ec22f1c0e4!8m2!3d41.3828139!4d2.1782045"
-      />
+      <div id="evento">
+        <div className="event-header">
+          <div className="event-title">THE WEDDING</div>
+          <div className="event-description">The big day!</div>
+        </div>
+        <div className="event-content">
+          <div className="event-media"><img className="event-image" src={Iglesia}></img></div>
+          <div className="event-details">
+            <div className="info-title"><h3>The Ceremony</h3></div>
+            <div className="info">
+              <div className="info-text">
+                <div ><Cal className="icon" /></div>
+                <div className="time-date">12:30h, Sunday, January 19th, 2025</div></div>
+              <div className="info-text">
+                <div><Pin className="icon"/></div>
+                <div>
+                  <div className="location"><h4>H Square Wedding Hall</h4></div>
+                  <div className="address">22-2, Majo-ro, Seongdong-gu, Seoul</div>
+                </div>
+              </div>
+              <div className="info-text">
+                <div><Map className="icon"/></div>
+                <div className="map"><a href="https://www.google.com/maps/place/H%EC%8A%A4%ED%80%98%EC%96%B4%EC%9B%A8%EB%94%A9%ED%99%80/data=!3m1!4b1!4m6!3m5!1s0x357ca5d4362571af:0xd64cdd7da9216d29!8m2!3d37.5594167!4d127.0414041!16s%2Fg%2F11rfbzds2j?authuser=0&entry=ttu" target="_blank">Map and directions</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Share />
       <Footer
         style={{
