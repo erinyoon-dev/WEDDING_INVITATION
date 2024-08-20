54dc676696e5b74b533f991dc8fcc5bf40efd4cf
erinyoon-dev <53050919+erinyoon-dev@users.noreply.github.com>
2024-08-21 02:22:34 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: `weddingInvitation`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
-  plugins: ["gatsby-plugin-styled-components"]
-};
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-7383220-8",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
