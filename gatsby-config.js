b8c6003c9f4f8857026c4b1badd8300e1b7a7aa6
erinyoon-dev <53050919+erinyoon-dev@users.noreply.github.com>
2024-08-21 02:26:07 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,23 +4,4 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-7383220-8",
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
 }
