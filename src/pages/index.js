41a6e2c353ff52a56fc17d332bf19c99052f7b4c
erinyoon-dev <53050919+erinyoon-dev@users.noreply.github.com>
2024-08-21 00:30:58 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,9 @@ import GroovePaper from "../assets/GroovePaper.png";
 import Song from "../assets/song.mp3";
 import Masia from "../assets/masia.png";
 import Iglesia from "../assets/iglesia.png";
+import Cal from "../assets/cal.png";
+import Pin from "../assets/pin.png";
+import Map from "../assets/map.png";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -39,7 +42,7 @@ const IndexPage = () => {
       <audio autoPlay loop>
         <source src={Song} />
       </audio>
-    /* Title */
+    // Titie
       <div className="hero">
         <div className="hero-content">
           <div className="hero-text">
@@ -51,7 +54,7 @@ const IndexPage = () => {
           </div>
         </div>
       </div>
-    /* Event */
+    // Event
       <div id="evento">
         <div className="event-header">
           <div className="event-title">THE WEDDING</div>
@@ -72,6 +75,30 @@ const IndexPage = () => {
               </div>
               <div className="info-text">
                 <div className="map"><a href="https://www.google.com/maps/place/H%EC%8A%A4%ED%80%98%EC%96%B4%EC%9B%A8%EB%94%A9%ED%99%80/data=!3m1!4b1!4m6!3m5!1s0x357ca5d4362571af:0xd64cdd7da9216d29!8m2!3d37.5594167!4d127.0414041!16s%2Fg%2F11rfbzds2j?authuser=0&entry=ttu" target="_blank">Map and directions</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="evento">
+       <div className="event-content">
+          <div className="event-media"><img className="event-image" src={Masia}></img></div>
+          <div className="event-details">
+            <div className="info-title"><h3>The Celebration</h3></div>
+            <div className="info">
+              <div className="info-text">
+                <div ><Cal className="icon" /></div>
+                <div className="time-date">15:00h, Sunday, January 19th, 2025</div></div>
+              <div className="info-text">
+                <div><Pin className="icon"/></div>
+                <div>
+                  <div className="location"><h4>To be updated</h4></div>
+                  <!-- <div className="address">To be updated</div> -->
+                </div>
+              </div>
+              <div className="info-text">
+                <div><Map className="icon"/></div>
+                <!-- <div className="map"><a href={this.props.mapLink} target="_blank">{this.props.map}</a></div> -->
               </div>
             </div>
           </div>
